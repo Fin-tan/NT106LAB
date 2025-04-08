bf514b215f81e1d8847b47c41e11d63ccd201540
Nhân Nguyễn Huỳnh <23521080@ms.uit.edu.vn>
2025-04-08 08:56:16 +0700

    new

--- LAB2/LAB2/Form2.cs
+++ LAB2/LAB2/Form2.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
+
 namespace lab2
 {
     public partial class Form2 : Form
@@ -19,19 +15,33 @@ namespace lab2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openfile = new OpenFileDialog();
-            if (openfile.ShowDialog()==DialogResult.OK)
+            using (OpenFileDialog openFile = new OpenFileDialog())
             {
-                StreamReader reader = new StreamReader(openfile.FileName,true);
-                string content = reader.ReadToEnd();
-                textBox1.Text = content;
-                textBox2.Text = openfile.SafeFileName.ToString();
-                textBox4.Text = openfile.FileName;
-                textBox6.Text = content.Length.ToString();
-                int linecount = File.ReadLines(openfile.FileName).Count();
-                textBox3.Text = linecount.ToString();
-                string[] source = content.Split(new char[] { '\r', '\n', '\t', '.', '?', '!', ' ', ':', ';', ',' },StringSplitOptions.RemoveEmptyEntries);
-                textBox5.Text = source.Count().ToString();
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                   
+                    string fileContent;
+                    using (StreamReader reader = new StreamReader(openFile.FileName, Encoding.UTF8))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+
+                  
+                    textBox1.Text = fileContent;
+                    textBox2.Text = openFile.SafeFileName;
+                    textBox4.Text = openFile.FileName;
+                    textBox6.Text = fileContent.Length.ToString();
+
+                   
+                    string[] lines = fileContent.Split(new[] {"\rn", "\n" }, StringSplitOptions.None);
+                    int realLineCount = lines.Count(line => !string.IsNullOrWhiteSpace(line));
+                    textBox3.Text = realLineCount.ToString();
+
+                   
+                    char[] wordDelimiters = { ' ', '\r', '\n', '\t', '.', '?', '!', ':', ';', ',' };
+                    int wordCount = fileContent.Split(wordDelimiters, StringSplitOptions.RemoveEmptyEntries).Length;
+                    textBox5.Text = wordCount.ToString();
+                }
             }
         }
     }
